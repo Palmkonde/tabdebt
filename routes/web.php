0bd33206497d4e941cbf4e6b31aae5c81534d728
PalmPalmPalmPalmPalm <chitipat.c2548@gmail.com>
2026-03-03 15:16:50 +0700

    [Refactor] router use controller

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::get('/', App\Http\Controllers\WelcomeController::class)->name('home');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
-
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
 
 require __DIR__.'/auth.php';
