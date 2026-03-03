868a4611d1ae50ad3a30c8a6110b611448bd7a96
PalmPalmPalmPalmPalm <chitipat.c2548@gmail.com>
2026-03-03 15:53:16 +0700

    [Add] Tags into DataSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\Group;
+use App\Models\Tag;
 use App\Models\Website;
 use App\Models\User;
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -17,13 +19,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $tags = Tag::factory()->count(20)->create();
         User::factory(10)
             ->has(
                 Group::factory()
-                    ->count(3)
-                    ->has(
-                        Website::factory()->count(5)
+                    ->hasAttached(
+                        $tags->random(3)
                     )
+                    ->has(
+                        Website::factory()
+                            ->hasAttached(
+                                $tags->random(2)
+                            )
+                            ->count(5)
+                    )
+                    ->count(3)
             )
             ->create();
     }
