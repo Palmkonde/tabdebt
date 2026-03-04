738d679634b6ca9d697f135db01ddb7d568f5fd4
PalmPalmPalmPalmPalm <chitipat.c2548@gmail.com>
2026-03-04 20:24:42 +0700

    [Add] websites count and Group count

--- app/Http/Controllers/WorkspaceController.php
+++ app/Http/Controllers/WorkspaceController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Website;
 
 class WorkspaceController extends Controller
 {
     public function index()
     {
+        $user = auth()->user();
         return view('workspace.index', [
-            'name' => auth()->user()->name
+            'name' => $user->name,
+            'websiteCount' => Website::whereIn('group_id', $user->group()->pluck('id'))->count(),
+            'groupCount' => $user->group()->count(),
         ]);
     }
 }
