d638ed36663f38df41554b4fac8df450cf1fd72a
PalmPalmPalmPalmPalm <chitipat.c2548@gmail.com>
2026-03-04 20:26:20 +0700

    [Add] workspace.index

--- resources/views/workspace/index.blade.php
+++ resources/views/workspace/index.blade.php
@@ -1,4 +1,8 @@
-<div>
-    <h1>{{ $name }}'s Workspace</h1>
-    <p>Welcome to your workspace!</p>
-</div>
+<h1>{{ $name }}'s Workspace</h1>
+<p> Welcome to your workspace, {{ $name }}! Here you can manage your websites and view your status. </p>
+
+<section>
+    <h2> Status </h2>
+    <x-status countNum="{{ $websiteCount }}" label="Websites" />
+    <x-status countNum="{{ $groupCount }}" label="Groups" />
+</section>
