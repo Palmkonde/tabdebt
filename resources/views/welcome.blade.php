a085fdbc166a09dbdf34f61778764dd5bbf949eb
PalmPalmPalmPalmPalm <chitipat.c2548@gmail.com>
2026-03-05 09:50:41 +0700

    [Refactor] Using tailwindCSS cdn instead of local installation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
         <!-- Styles -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     </head>
     <body class="antialiased font-sans">
         <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
